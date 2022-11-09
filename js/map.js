2407507c124660bd003b7ebb5f2cca7fb0f73d7c
Максим Щенников <championgbi@yandex.ru>
2022-11-09 22:44:35 +0300

    задание Правда или действие (часть 2) + карта

---
+++ js/map.js
@@ -0,0 +1,95 @@
+import {activeForm} from './form.js';
+import {renderAccomodationOffer} from './card.js';
+import {createObjects} from './data.js';
+
+const map = L.map('map-canvas');
+const resetButton = document.querySelector('.ad-form__reset');
+const address = document.querySelector('#address');
+
+const TOKYO_CENTER = {
+  lat: 35.68091,
+  lng: 139.76529
+};
+
+const MAP_ZOOM = 10;
+
+const loadMap = () => {
+  map.on('load', () => {
+    activeForm();
+  })
+    .setView({
+      lat: TOKYO_CENTER.lat,
+      lng: TOKYO_CENTER.lng,
+    }, MAP_ZOOM);
+
+  L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    },
+  ).addTo(map);
+};
+
+const mainPin = L.icon({
+  iconUrl: './img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const pin = L.icon({
+  iconUrl: './img/pin.svg',
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+});
+
+const createPin = (point) => {
+  const {lat, lng} = point.location;
+  const marker = L.marker(
+    {
+      lat,
+      lng
+    },
+    {
+      pin,
+    },
+  );
+  marker
+    .addTo(map)
+    .bindPopup(renderAccomodationOffer(point));
+};
+
+createObjects().forEach((point) => {
+  createPin(point);
+});
+const mainMarker = L.marker(
+  {
+    lat: TOKYO_CENTER.lat,
+    lng: TOKYO_CENTER.lng,
+  },
+  {
+    draggable: true,
+    icon: mainPin
+  }
+);
+
+mainMarker.addTo(map);
+
+resetButton.addEventListener('click', () => {
+  mainMarker.setLatLng({
+    lat: TOKYO_CENTER.lat,
+    lng: TOKYO_CENTER.lng,
+  });
+  map.setView({
+    lat: TOKYO_CENTER.lat,
+    lng: TOKYO_CENTER.lng,
+  }, MAP_ZOOM);
+});
+
+
+mainMarker.on('moveend', (evt) => {
+  const newAddress = evt.target.getLatLng();
+  address.value = `lat:${newAddress.lat}, lng:${newAddress.lng}`;
+});
+
+export {loadMap};
+
